a0c71669faeef2eba07262d3ab1c0f5fd24c9a61
Jonathan Kim <kjonny3@gmail.com>
2024-05-14 21:46:50 -0400

    Create popup.js
    popup.js

---
+++ extension/popup.js
@@ -0,0 +1,7 @@
+// This script could be used for any JavaScript functionality you need in your popup window
+
+// Example: Add event listener for when the popup is loaded
+document.addEventListener('DOMContentLoaded', function() {
+    // Perform any actions you need here
+    console.log('Popup loaded!');
+});
